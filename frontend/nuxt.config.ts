f63fb15e67423569cc0fb34fcc96899ed85dc4f9
Hasban-Fardani <hasbanfardani77@gmail.com>
2024-09-03 13:32:21 +0700

    add nuxt ui, add sanctum setting on frontend

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ['nuxt-auth-sanctum', '@nuxt/image', '@nuxt/fonts']
-})
+  modules: [
+    "nuxt-auth-sanctum", 
+    "@nuxt/image", 
+    "@nuxt/fonts", 
+    "@nuxt/ui"
+  ],
+
+  sanctum: {
+    endpoints: {
+      user: '/me'
+    }
+  }
+});
